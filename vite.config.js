f55692d59f19ccdd9ab3499bc8ed0142d0710dba
momoshell <xtopala+cryptogit@gmail.com>
2026-03-21 17:34:24 +0100

    fix: allow Vite to serve content folder outside src

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: [resolve(__dirname, 'content')]
+		}
+	}
 });
